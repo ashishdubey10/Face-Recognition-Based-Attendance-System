c9afa977d21b2730869662f7c2d011c7d1bf93b6
vinay316 <37390193+vinay316@users.noreply.github.com>
2020-10-01 00:14:08 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Jun 17 2018
 
-@author: Ashish Kumar
+@author: vinay
 """
 
 import tkinter as tk
@@ -63,30 +63,30 @@ message = tk.Label(window, text="Face-Recognition-Based-Attendance-Management-Sy
 
 message.place(x=200, y=20)
 
-lbl = tk.Label(window, text="Enter ID",width=20  ,height=2  ,fg="red"  ,bg="yellow" ,font=('times', 15, ' bold ') ) 
-lbl.place(x=400, y=200)
+lbl = tk.Label(window, text="Enter ID",width=20  ,height=2  ,fg="red"  ,bg="yellow" ,font=('times', 15, ' bold ') )
+lbl.place(x=200, y=200)
 
-txt = tk.Entry(window,width=20  ,bg="yellow" ,fg="red",font=('times', 15, ' bold '))
-txt.place(x=700, y=215)
+txt = tk.Entry(window,width=25  ,bg="yellow" ,fg="red",font=('times', 15, ' bold '))
+txt.place(x=550, y=215)
 
-lbl2 = tk.Label(window, text="Enter Name",width=20  ,fg="red"  ,bg="yellow"    ,height=2 ,font=('times', 15, ' bold ')) 
-lbl2.place(x=400, y=300)
+lbl2 = tk.Label(window, text="Enter Name",width=20  ,fg="red"  ,bg="yellow"    ,height=2 ,font=('times', 15, ' bold '))
+lbl2.place(x=200, y=275)
 
-txt2 = tk.Entry(window,width=20  ,bg="yellow"  ,fg="red",font=('times', 15, ' bold ')  )
-txt2.place(x=700, y=315)
+txt2 = tk.Entry(window,width=25  ,bg="yellow"  ,fg="red",font=('times', 15, ' bold ')  )
+txt2.place(x=550, y=290)
 
-lbl3 = tk.Label(window, text="Notification : ",width=20  ,fg="red"  ,bg="yellow"  ,height=2 ,font=('times', 15, ' bold underline ')) 
-lbl3.place(x=400, y=400)
+lbl3 = tk.Label(window, text="Notification : ",width=20  ,fg="red"  ,bg="yellow"  ,height=2 ,font=('times', 15, ' bold underline '))
+lbl3.place(x=200, y=375)
 
-message = tk.Label(window, text="" ,bg="yellow"  ,fg="red"  ,width=30  ,height=2, activebackground = "yellow" ,font=('times', 15, ' bold ')) 
-message.place(x=700, y=400)
+message = tk.Label(window, text="" ,bg="yellow"  ,fg="red"  ,width=50  ,height=2, activebackground = "yellow" ,font=('times', 15, ' bold '))
+message.place(x=550, y=375)
 
-lbl3 = tk.Label(window, text="Attendance : ",width=20  ,fg="red"  ,bg="yellow"  ,height=2 ,font=('times', 15, ' bold  underline')) 
-lbl3.place(x=400, y=650)
+lbl3 = tk.Label(window, text="Attendance : ",width=20  ,fg="red"  ,bg="yellow"  ,height=2 ,font=('times', 15, ' bold  underline'))
+lbl3.place(x=200, y=625)
 
 
-message2 = tk.Label(window, text="" ,fg="red"   ,bg="yellow",activeforeground = "green",width=30  ,height=2  ,font=('times', 15, ' bold ')) 
-message2.place(x=700, y=650)
+message2 = tk.Label(window, text="" ,fg="red"   ,bg="yellow",activeforeground = "green",width=50  ,height=4  ,font=('times', 15, ' bold '))
+message2.place(x=550, y=600)
  
 def clear():
     txt.delete(0, 'end')    
